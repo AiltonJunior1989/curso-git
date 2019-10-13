b4b1cf1d3f2988904b8cb41c66cc8fef243338a9
Ailton Júnior <ailtonjesusjunior@gmail.com>
2019-10-13 11:07:07 -0300

    Acrescentada uma mensagem de bom dia

--- ProjetoTeste/Program.cs
+++ ProjetoTeste/Program.cs
@@ -4,6 +4,7 @@ namespace ProjetoTeste {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Ola mundo!");
+            Console.WriteLine("Bom dia!");
             Console.ReadLine();
         }
     }
